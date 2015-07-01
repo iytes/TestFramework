aababb2ae65dba4a6567f1dc923e644812a34e00
Yasuhiro Inami <inamiy@gmail.com>
2015-07-01 17:30:54 +0900

    comment-out force-fail test

--- TestFrameworkTests/TestFrameworkTests.swift
+++ TestFrameworkTests/TestFrameworkTests.swift
@@ -25,7 +25,7 @@ class TestFrameworkTests: XCTestCase {
         // This is an example of a functional test case.
         XCTAssert(true, "Pass")
         
-        XCTFail("Force Fail!")
+//        XCTFail("Force Fail!")
     }
     
     func testPerformanceExample() {
